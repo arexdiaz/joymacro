53648ae0cc94e75dcadc8e6c6139934cd76dd26d
arexdiaz <arexito@icloud.com>
2025-03-08 11:33:22 -0400

    Refactor code

--- overlay.py
+++ overlay.py
@@ -169,7 +169,6 @@ class OverlayWindow(QMainWindow):
         self.removeWindow(window) # If button is not removed after sigkill, refactor this
 
     def addWindow(self, window):
-        self.winman_container.createButton(f"{window["binary_name"]}[{window["pid"]}]", partial(self.killProc, window))
         sub = self.createSubcontainer({window["binary_name"]}[{window["pid"]}], self.winman_container)
         sub.createButton("Fullscreen", partial(cmd.exec, f"wmctrl -i -r {window["window_id"]} -b toggle,fullscreen"))
         sub.createButton("Maximize", partial(cmd.exec, f"wmctrl -i -r {window["window_id"]} -b toggle,maximized_vert,maximized_horz"))
