1366b84327b2b2feff9891f28f9dd8fe9f531dae
arexdiaz <arexito@icloud.com>
2025-03-14 14:26:40 -0400

    Refactor code

--- overlay.py
+++ overlay.py
@@ -163,6 +163,9 @@ class OverlayWindow(QMainWindow):
         else:
             battery_status = ""
 
+        index = exec("echo -n $(echo $(sudo /usr/sbin/nvpmodel -q) | awk 'END{print $NF}')").stdout.strip()
+        self.profile_container.current_profile = self.profile_container.profiles[index]
+
         self.cm.getContainer("primary_container").getWidget("hwstat").widget().setText(
                 f"{current_date} {current_time}\n"\
                 f"{user}@{host_name}\n"\
