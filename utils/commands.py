dbbd610fff550c7d911300954cab6f275bdbfc88
arexdiaz <arexito@icloud.com>
2025-03-27 01:20:17 -0400

    Update code

--- utils/commands.py
+++ utils/commands.py
@@ -78,24 +78,25 @@ def onWindowOpen(self, window):
         ("Fullscreen", window.toggleFullscreen),
         ("Maximize", window.toggleMaximize),
         ("Minimize", window.toggleMinimize),
+        ("Focus", window.setFocus),
         ("Close", window.close),
     ]
     extra_actions = [
-        ("Set Focus", window.setFocus),
         ("Keep Above Others", window.toggleAlwaysOnTop),
-        ("No Tilebar and Frame", window.toggleNoTitlebarFrame),
+        ("No Tilebar and Frame (WIP)", window.toggleNoTitlebarFrame),
     ]
+
     for label, action in actions:
         child_container.createButton(label, action)
-
-    extra_container = child_container.createChildContainer("More Actions")
-    for label, action in extra_actions:
-        extra_container.createButton(label, action)
-    extra_container.populateContainer()
 
     stats_container = child_container.createChildContainer("debug_info (WIP)", pos="bottom")
     stats_container.is_init = False
     stats_container.populateContainer()
+
+    extra_container = child_container.createChildContainer("More Actions", pos="bottom")
+    for label, action in extra_actions:
+        extra_container.createButton(label, action)
+    extra_container.populateContainer()
 
     child_container.populateContainer()
     self.removeWidget("empty")
